6b2ec0b7f28c025f69340b93e3264ae44a28a5b5
Daniel Esuola <esuoladaniel002@gmail.com>
2022-11-11 16:11:58 +0100

    adding yup schemas

--- src/schemas/validation.schema.js
+++ src/schemas/validation.schema.js
@@ -1,9 +1,8 @@
 import * as yup from "yup";
 
-const passworddRegex = ''
-
-const signUpSchema = yup.object().shape({
-    name: yup.string().required(),
-    email: yup.string().email('Enter a valid email address').required(),
-    password: yup.string().password('')
+export const signUpSchema = yup.object().shape({
+    name: yup.string().required('Required'),
+    email: yup.string().email('Enter a valid email address').required('Required'),
+    password: yup.string().min(5).required('Required'), //Minimum of 5 characters
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required('Required')
 })
